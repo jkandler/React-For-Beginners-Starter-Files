9641d39a798351a76772681710871d0999b89366
Julia Kandler <julia.kandler@produktmacher.com>
2018-04-13 16:42:29 +0200

    Finish Lesson13

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,14 +4,31 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = (fish) => {
+    console.log('adding a fish');
+    // take a copy of the existing state
+    const fishes = {...this.state.fishes};
+    // add new fish to that fishes variable
+    fishes[`fish${Date.now()}`] = fish
+    // set the new fishes object to state
+    this.setState({
+      fishes: fishes
+    })
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header />
+          <Header tagline="Fresh Seafood Market"/>
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish}/>
       </div>
     )
   }
--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import {getFunName} from '../helpers'
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = (event) => {
+    // stop the form from submitting
+    event.preventDefault();
+    // get text from input
+    const storeName = this.myInput.value.value;
+    // change the page to /store/whatever-they-entered
+    this.props.history.push(`/store/${storeName}`);
+  }
+
   render() {
     return (
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
           <h2>Please enter a Store</h2>
-          <input type="test" required placeholder="Store Name"/>
+          <input
+            type="text"
+            ref={this.myInput}
+            required
+            placeholder="Store Name"
+            defaultValue={getFunName()}
+          />
           <button type="submit">Visit Store --></button>
         </form>
     )
