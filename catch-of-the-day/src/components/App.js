76158b8fd6a5ef3adde18933f02cb0171768e09a
Julia Kandler <julia.kandler@produktmacher.com>
2018-04-15 10:41:22 +0200

    set states and props

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import Fish from './Fish';
+import sampleFishes from '../sample-fishes'
 
 class App extends React.Component {
   state = {
@@ -21,14 +23,40 @@ class App extends React.Component {
     })
   }
 
+  loadSampleFishes = () => {
+    this.setState({fishes: sampleFishes});
+  }
+
+  addToOrder = key => {
+    // take a copa of state
+    const order = {...this.state.order};
+    // either add to order or update the number in order
+    order[key] = order[key] + 1 || 1;
+    // call setState to update our state Object
+    this.setState({order: order})
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market"/>
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key =>
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />)
+            }
+          </ul>
         </div>
         <Order />
-        <Inventory addFish={this.addFish}/>
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
     )
   }
